03eb2241095f1a9609a9d5e5ca4c5c99ea2cb09a
Prabhat Patel <patelprabhat145@gmail.com>
2025-08-15 09:13:15 +0530

    After little changes

--- name-app/src/Namedisplay.jsx
+++ name-app/src/Namedisplay.jsx
@@ -7,33 +7,38 @@ export default function Namedisplay(){
 
     const handleSubmit=(e)=>{
         e.preventDefault();
-        // setFullName(`${firstName} ${lastName}`);
+        if (!firstName.trim() || !lastName.trim()) {
+      setFullName("");
+      return;
+    }
+           // setFullName(`${firstName} ${lastName}`);
         setFullName(`${firstName} ${lastName}`);
-
     }
     return (
         <div>
       <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
-      <label htmlFor="firstName">First Name :</label>
+      <label htmlFor="firstName">First Name:</label>
     <input 
       id='firstName'
       name='firstName'
+      type='text'
     onChange={(e)=>setFirstName(e.target.value)}
     required />   
      <br />
-    <label htmlFor='lastName' >Last Name : </label>
+    <label htmlFor='lastName' >Last Name:</label>
     <input 
     id='lastName'
     name='lastName'
-    onChange={(e)=>setLastName(e.target.value)} />
+    type='text'
+    onChange={(e)=>setLastName(e.target.value)}
+    required />
     <br />
     <button type='submit' >Submit</button>
     </form>
     <div>
-       { fullName &&<p>Full Name : {fullName}</p>}
+       {fullName &&<p>Full Name: {fullName}</p>}
     </div>
-
         </div>
     )
 
